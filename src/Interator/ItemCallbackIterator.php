9465980966141ab0bf045652c483f25177b49afc
Matijs de Jong <mjong@magnafacta.nl>
2023-07-10 15:05:00 +0200

    Add ItemCallbackIterator (for model)

---
+++ src/Interator/ItemCallbackIterator.php
@@ -0,0 +1,120 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @package    Zalt
+ * @subpackage Interator
+ * @author     Matijs de Jong <mjong@magnafacta.nl>
+ */
+
+namespace Zalt\Interator;
+
+/**
+ * @package    Zalt
+ * @subpackage Interator
+ * @since      Class available since version 1.0
+ */
+class ItemCallbackIterator implements \OuterIterator, \Countable
+{
+    /**
+     *
+     * @var callable
+     */
+    private $_callback;
+
+    /**
+     *
+     * @var \Iterator
+     */
+    private $_iterator;
+
+    /**
+     *
+     * @param \Traversable $iterator
+     * @param Callable $callback
+     */
+    public function __construct(\Traversable $iterator, callable $callback)
+    {
+        $this->_iterator = $iterator;
+        while ($this->_iterator instanceof \IteratorAggregate) {
+            $this->_iterator = $this->_iterator->getIterator();
+        }
+
+        $this->_callback = $callback;
+    }
+
+    /**
+     * Count elements of an object
+     *
+     * Rewinding version of count
+     *
+     * @return int
+     */
+    public function count(): int
+    {
+        if ($this->_iterator instanceof \Countable) {
+            return $this->_iterator->count();
+        }
+
+        $count = iterator_count($this->_iterator);
+        $this->_iterator->rewind();
+        return $count;
+    }
+
+    /**
+     * Return the current element
+     *
+     * @return mixed
+     */
+    public function current(): mixed
+    {
+        return call_user_func($this->_callback, $this->_iterator->current());
+    }
+
+    /**
+     * Returns the inner iterator for the current entry.
+     *
+     * @return \Iterator
+     */
+    public function getInnerIterator(): ?\Iterator
+    {
+        return $this->_iterator;
+    }
+
+    /**
+     * Return the key of the current element
+     *
+     * @return mixed
+     */
+    public function key(): mixed
+    {
+        return $this->_iterator->key();
+    }
+
+    /**
+     * Move forward to next element
+     */
+    public function next(): void
+    {
+        $this->_iterator->next();
+    }
+
+    /**
+     * Rewind the \Iterator to the first element
+     */
+    public function rewind(): void
+    {
+        $this->_iterator->rewind();
+    }
+
+    /**
+     * Checks if current position is valid
+     *
+     * @return boolean
+     */
+    public function valid(): bool
+    {
+        return $this->_iterator->valid();
+    }
+}
