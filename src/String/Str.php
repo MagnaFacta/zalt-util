26f25de729a62e35ca00ba9dcb04b6e69c7fb4d8
Jasper van Gestel <jvangestel@gmail.com>
2024-02-14 10:11:13 +0100

    new string functions

--- src/String/Str.php
+++ src/String/Str.php
@@ -4,6 +4,52 @@ namespace Zalt\String;
 
 class Str
 {
+
+    /**
+     * The cache of camel-cased words.
+     *
+     * @var array
+     */
+    protected static $camelCache = [];
+
+    /**
+     * The cache of snake-cased words.
+     *
+     * @var array
+     */
+    protected static $snakeCache = [];
+
+    /**
+     * The cache of studly-cased words.
+     *
+     * @var array
+     */
+    protected static $studlyCache = [];
+
+    public static function alphaNum(string $value, bool $allowSpaces = false): string
+    {
+        $regex = '/[^a-z\d]/i';
+        if ($allowSpaces) {
+            $regex = '/[^a-z\d ]/i';
+        }
+        return preg_replace($regex, '', $value);
+    }
+
+    /**
+     * Convert a value to camel case.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public static function camel($value)
+    {
+        if (isset(static::$camelCache[$value])) {
+            return static::$camelCache[$value];
+        }
+
+        return static::$camelCache[$value] = lcfirst(static::studly($value));
+    }
+
     /**
      * Convert a string to kebab case.
      */
@@ -13,25 +59,90 @@ class Str
     }
 
     /**
+     * A UTF-8 safe version of strtolower()
+     */
+    public static function lower(string $string = null): string
+    {
+        return mb_strtolower($string ?? '', 'UTF-8');
+    }
+
+    public static function pascal(string $value): string
+    {
+        return static::studly($value);
+    }
+
+    /**
+     * Generate a URL friendly "slug" from a given string.
+     *
+     * @param  string  $title
+     * @param  string  $separator
+     * @param  string|null  $language
+     * @param  array<string, string>  $dictionary
+     * @return string
+     */
+    public static function slug($title, $separator = '-', $language = 'en', $dictionary = ['@' => 'at'])
+    {
+        // Convert all dashes/underscores into separator
+        $flip = $separator === '-' ? '_' : '-';
+
+        $title = preg_replace('!['.preg_quote($flip).']+!u', $separator, $title);
+
+        // Replace dictionary words
+        foreach ($dictionary as $key => $value) {
+            $dictionary[$key] = $separator.$value.$separator;
+        }
+
+        $title = str_replace(array_keys($dictionary), array_values($dictionary), $title);
+
+        // Remove all characters that are not the separator, letters, numbers, or whitespace
+        $title = preg_replace('![^'.preg_quote($separator).'\pL\pN\s]+!u', '', static::lower($title));
+
+        // Replace all separator characters and whitespace by a single separator
+        $title = preg_replace('!['.preg_quote($separator).'\s]+!u', $separator, $title);
+
+        return trim($title, $separator);
+    }
+
+    /**
      * Convert a string to snake case.
      */
     public static function snake(
         string $value = null,
         string $delimiter = '_'
     ): string {
-        if (ctype_lower($value) === false) {
-            $value = preg_replace('/[\s\\\]+/u', '', ucwords($value));
-            $value = preg_replace('/(.)(?=[A-Z])/u', '$1' . $delimiter, $value);
-            $value = static::lower($value);
+        $key = $value;
+
+        if (isset(static::$snakeCache[$key][$delimiter])) {
+            return static::$snakeCache[$key][$delimiter];
         }
-        return $value;
+
+        if (! ctype_lower($value)) {
+            $value = preg_replace('/\s+/u', '', ucwords($value));
+
+            $value = static::lower(preg_replace('/(.)(?=[A-Z])/u', '$1'.$delimiter, $value));
+        }
+
+        return static::$snakeCache[$key][$delimiter] = $value;
     }
 
     /**
-     * A UTF-8 safe version of strtolower()
+     * Convert a value to studly caps case.
+     *
+     * @param  string  $value
+     * @return string
      */
-    public static function lower(string $string = null): string
+    public static function studly($value)
     {
-        return mb_strtolower($string ?? '', 'UTF-8');
+        $key = $value;
+
+        if (isset(static::$studlyCache[$key])) {
+            return static::$studlyCache[$key];
+        }
+
+        $words = explode(' ', str_replace(['-', '_'], ' ', $value));
+
+        $studlyWords = array_map(fn ($word) => ucfirst($word), $words);
+
+        return static::$studlyCache[$key] = implode($studlyWords);
     }
 }
